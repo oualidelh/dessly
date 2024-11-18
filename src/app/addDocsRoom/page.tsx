b1ca015c093e3be1f802887d5284a3079b77520e
oualidelh <elhouarioualid@gmail.com>
2024-11-19 00:45:53 +0100

    typing features using zustand

--- src/app/addDocsRoom/page.tsx
+++ src/app/addDocsRoom/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -14,14 +14,50 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { supabase } from "../../../utils/supabaseClient";
+import useStore from "../store";
 
 const formSchema = z.object({
   fullName: z.string().min(2).max(50),
   profession: z.string().min(2).max(50),
 });
 
+function WhoIsHere() {
+  const othersUsersCount = useStore((state) => state.liveblocks.others.length);
+
+  return (
+    <div className="who_is_here">
+      There are {othersUsersCount} other users online
+    </div>
+  );
+}
+
+function SomeoneIsTyping() {
+  const others = useStore((state) => state.liveblocks.others);
+
+  const someoneIsTyping = others.some((user) => user.presence.isTyping);
+
+  return someoneIsTyping ? (
+    <div className="someone_is_typing">Someone is typing</div>
+  ) : null;
+}
+
 const AddDocsRoom = () => {
   const [formError, setFormError] = useState<string | null>(null);
+  const {
+    fullName,
+    setFullName,
+    liveblocks: { enterRoom, leaveRoom },
+  } = useStore();
+
+  console.log("fullname", fullName)
+
+  useEffect(() => {
+    enterRoom("zustand-add-doc");
+    return () => {
+      leaveRoom("zustand-add-doc");
+    };
+  }, [enterRoom, leaveRoom]);
+  
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -59,11 +95,18 @@ const AddDocsRoom = () => {
                 <FormLabel className="shad-form-label">FullName</FormLabel>
                 <FormControl>
                   <Input
+                  {...field}
+                  onChange={(e) => {
+                    field.onChange(e); // Let React Hook Form handle value updates
+                    setFullName(e.target.value); // Update the Zustand state or perform any custom logic
+                  }}
+                  value={fullName} // Ensure the input reflects the controlled value
+        
                     className="shad-input"
-                    placeholder="EX: Oualid Elhouari"
-                    {...field}
+                    placeholder="EX: Oualid Elhouari" 
                   />
                 </FormControl>
+                <SomeoneIsTyping />
                 <FormMessage />
               </FormItem>
             )}
@@ -94,6 +137,7 @@ const AddDocsRoom = () => {
         </form>
       </Form>
       {formError && <p className="text-red-500 mt-4">{formError}</p>}
+      <WhoIsHere />
     </div>
   );
 };
