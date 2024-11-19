a69872d907f8373e181ba522fbe480ee918becf5
oualidelh <elhouarioualid@gmail.com>
2024-11-19 22:28:34 +0100

    add istyping to bouth inputs

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,31 +2,85 @@ import create from "zustand";
 import { createClient } from "@liveblocks/client";
 import { liveblocks, WithLiveblocks } from "@liveblocks/zustand";
 
+// type Presence = {
+//     isTyping: boolean;
+//   };
+
+// type State = {
+//     profession: string;
+//     fullName: string;
+//     isTyping: boolean;
+//     setFullName: (fullName: string) => void;
+//     setProfession: (profession: string) => void;
+//   };
+
+// const client = createClient({
+//   publicApiKey: "pk_prod_gCyerjnDvITjjjdx0LtsI1tVhtTyHf9PFOkTsBQRcxY_qCcOUYFZ4G25vJEpjnKG",
+// });
+
+// const useStore = create<WithLiveblocks<State, Presence>>()(
+//     liveblocks(
+//       (set) => ({
+//         fullName: "",
+//         profession:"",
+//         isTyping: false,
+//         setFullName: (fullName) => set({ fullName, isTyping: fullName !== "" }),
+//         setProfession: (profession) => set({ profession, isTyping: profession !== "" }),
+//       }),
+//       { client,
+//         presenceMapping: { isTyping: true,},
+//        }
+//     )
+//   );
+
+// export default useStore;
+
+
 type Presence = {
-    isTyping: boolean;
+    isTypingFullName: boolean;
+    isTypingProfession: boolean;
   };
-
-type State = {
+  
+  type State = {
     fullName: string;
-    isTyping: boolean;
+    profession: string;
+    isTypingFullName: boolean;
+    isTypingProfession: boolean;
     setFullName: (fullName: string) => void;
+    setProfession: (profession: string) => void;
   };
-
-const client = createClient({
-  publicApiKey: "pk_prod_gCyerjnDvITjjjdx0LtsI1tVhtTyHf9PFOkTsBQRcxY_qCcOUYFZ4G25vJEpjnKG",
-});
-
-const useStore = create<WithLiveblocks<State, Presence>>()(
+  
+  const client = createClient({
+    publicApiKey: "pk_prod_gCyerjnDvITjjjdx0LtsI1tVhtTyHf9PFOkTsBQRcxY_qCcOUYFZ4G25vJEpjnKG",
+  });
+  
+  const useStore = create<WithLiveblocks<State, Presence>>()(
     liveblocks(
       (set) => ({
         fullName: "",
-        isTyping: false,
-        setFullName: (fullName) => set({ fullName, isTyping: fullName !== "" }),
+        profession: "",
+        isTypingFullName: false,
+        isTypingProfession: false,
+        setFullName: (fullName) =>
+          set({
+            fullName,
+            isTypingFullName: fullName !== "",
+          }),
+        setProfession: (profession) =>
+          set({
+            profession,
+            isTypingProfession: profession !== "",
+          }),
       }),
-      { client,
-        presenceMapping: { isTyping: true },
-       }
+      {
+        client,
+        presenceMapping: {
+          isTypingFullName: true,
+          isTypingProfession: true,
+        },
+      }
     )
   );
-
-export default useStore;
+  
+  export default useStore;
+  
