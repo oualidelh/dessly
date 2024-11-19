c59e8e48d3afd52cfe92d642e6b1fe54173fca56
oualidelh <elhouarioualid@gmail.com>
2024-11-19 22:29:22 +0100

    add istyping to bouth inputs

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,39 +2,6 @@ import create from "zustand";
 import { createClient } from "@liveblocks/client";
 import { liveblocks, WithLiveblocks } from "@liveblocks/zustand";
 
-// type Presence = {
-//     isTyping: boolean;
-//   };
-
-// type State = {
-//     profession: string;
-//     fullName: string;
-//     isTyping: boolean;
-//     setFullName: (fullName: string) => void;
-//     setProfession: (profession: string) => void;
-//   };
-
-// const client = createClient({
-//   publicApiKey: "pk_prod_gCyerjnDvITjjjdx0LtsI1tVhtTyHf9PFOkTsBQRcxY_qCcOUYFZ4G25vJEpjnKG",
-// });
-
-// const useStore = create<WithLiveblocks<State, Presence>>()(
-//     liveblocks(
-//       (set) => ({
-//         fullName: "",
-//         profession:"",
-//         isTyping: false,
-//         setFullName: (fullName) => set({ fullName, isTyping: fullName !== "" }),
-//         setProfession: (profession) => set({ profession, isTyping: profession !== "" }),
-//       }),
-//       { client,
-//         presenceMapping: { isTyping: true,},
-//        }
-//     )
-//   );
-
-// export default useStore;
-
 
 type Presence = {
     isTypingFullName: boolean;
